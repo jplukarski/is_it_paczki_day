0382be0bc01bbb25433cc57460e0b13a768500a2
JPlukarski <jwplukarski@gmail.com>
2019-09-21 15:54:34 -0500

    Got this thing started

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import MenuBar from './components/menubar'
+import MainContent from './components/maincontent'
+
+function App() {
+  return (
+    <>
+      <MenuBar />
+      <MainContent />
+    </>
+  );
+}
+
+export default App;
